23acbc87963c5c4620a6eea203764a9b473e5f14
luks-prog <lucasanaliseds@gmail.com>
2022-03-10 22:11:06 -0300

    Finalizando - Explorando diretivas com o angular(dio)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Meu primeiro projeto Angular';
+export class AppComponent implements OnInit {
+  count = 0;
+  nome = 'Lucas Rodrigues Pascoal';
+  text = '';
+
+  pessoas = [
+    {
+      nome: "Ivonaldo",
+      sobrenome: "Soares"
+    },
+    {
+      nome: "Maria",
+      sobrenome: "Silva"
+    },
+    {
+      nome: "Joao",
+      sobrenome: "Pereira"
+    },
+    {
+      nome: "Marcio",
+      sobrenome: "Santos"
+    }
+  ]
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+    console.log(this.pessoas);
+    let interval = setInterval(() => {
+      this.count++;
+      if(this.count === 10) {
+        clearInterval(interval);
+      }
+    },1000)
+}
+
+clicou(nome: string): void {
+  console.log('Clicou em mim' , nome)
+}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { ButtonComponent } from './components/button/button.component';
+import { FormsModule } from '@angular/forms';
+import { MarcaTextoDirective } from './directives/marca-texto.directive';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ButtonComponent
+    ButtonComponent,
+    MarcaTextoDirective
   ],
   imports: [
     BrowserModule,
-    NoopAnimationsModule
+    NoopAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
